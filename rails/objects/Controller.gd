7e3712a841c6205334aca9922dea965da7666e9e
Patrickgh3 <patrickjtraynor@gmail.com>
2021-09-17 00:57:31 -0700

    Fix Axes puzzle, tweak rail flash visuals, make music loop

--- rails/objects/Controller.gd
+++ rails/objects/Controller.gd
@@ -31,10 +31,10 @@ func _ready():
 	volumes[$MusicRoot2Deep] = 0
 	volumes[$MusicRoot3Hope] = 0
 	volumes[$MusicGuitarEcho] = 0
-	music_active[$MusicRoot1Piano] = false
-	music_active[$MusicRoot2Deep] = true
-	music_active[$MusicRoot3Hope] = false
-	music_active[$MusicGuitarEcho] = true
+	music_active[$MusicRoot1Piano] = randf() < 0.5
+	music_active[$MusicRoot2Deep] = randf() < 0.5
+	music_active[$MusicRoot3Hope] = randf() < 0.5
+	music_active[$MusicGuitarEcho] = randf() < 0.5
 	
 	
 
@@ -46,6 +46,8 @@ func _process(_delta):
 	if rails_just_departed_timer == 2:
 		rails_just_departed.clear()
 		
+	# Gradually fade music layers in and out depending on which ones
+	# we want to be playing right now
 	for node in volumes.keys():
 		if music_active[node]:
 			volumes[node] += _delta
@@ -54,6 +56,7 @@ func _process(_delta):
 		volumes[node] = clamp(volumes[node], 0, 1)
 		node.set_volume_db(lerp(-30, 0, volumes[node]))
 	
+	# Test key to randomize music layers
 	if Input.is_key_pressed(KEY_U):
 		for node in volumes.keys():
 			music_active[node] = randf() < 0.5
