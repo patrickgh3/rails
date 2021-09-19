3a285a8a49a14d139092bfe73679f8d492fa554f
Patrickgh3 <patrickjtraynor@gmail.com>
2021-09-19 02:57:42 -0700

    Add new music, assign music per puzzle (feel free to change music assignments)

--- rails/objects/CubioSpawn.gd
+++ rails/objects/CubioSpawn.gd
@@ -1,10 +1,2 @@
 extends Spatial
 
-export(bool) var debug_start_here
-
-func _ready():
-	if debug_start_here:
-		print("Notice: starting the player at puzzle "+get_parent().name+ " due to CubioSpawn having debug_start_here checkbox set")
-		var controller = $"/root/Root/Controller"
-		controller.current_puzzle = get_parent()
-		controller.reset_puzzle()
--- rails/objects/EnterPuzzleTrigger.gd
+++ rails/objects/EnterPuzzleTrigger.gd
@@ -2,4 +2,4 @@ extends Spatial
 
 func _on_Area_body_entered(body):
 	if body is Cubio:
-		$"/root/Root/Controller".register_puzzle(self)
+		$"/root/Root/Controller".register_puzzle(self.get_parent())
